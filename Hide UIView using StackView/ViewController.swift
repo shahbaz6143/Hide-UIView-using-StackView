e2255188e4e22a2dd59a138bc70a69cd9e191552
shahbaz6143 <shahbazalam6143@gmail.com>
2020-11-29 15:56:15 +0530

    first commit

--- Hide UIView using StackView/ViewController.swift
+++ Hide UIView using StackView/ViewController.swift
@@ -10,11 +10,40 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var edit: UIButton!
+    @IBOutlet weak var bottomView: UIView!
+    @IBOutlet weak var tableview: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        tableview.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+
     }
 
-
+    @IBAction func editBtnClick(_ sender: UIButton) {
+        edit.setTitle("Done", for: .normal)
+        edit.addTarget(self, action: #selector(doneBtnClick), for: .touchUpInside)
+        self.bottomView.isHidden = true
+    
+    }
+    
+    @objc func doneBtnClick() {
+        edit.setTitle("Edit", for: .normal)
+        edit.addTarget(self, action: #selector(editBtnClick(_:)), for: .touchUpInside)
+        self.bottomView.isHidden = false
+    }
+    
 }
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+}
